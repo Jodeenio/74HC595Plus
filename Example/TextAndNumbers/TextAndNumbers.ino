05432d20a8e5310910bba20aab8a9e03499b2fe7
Jodeenio <78582141+Jodeenio@users.noreply.github.com>
2025-07-30 22:30:39 +1000

    Examples of displayTxt() and displayNumber() functions.

--- Example/TextAndNumbers/TextAndNumbers.ino
+++ Example/TextAndNumbers/TextAndNumbers.ino
@@ -1,1 +1,13 @@
+#include <74HC595Plus.h>
 
+HC595Plus display(12, 11, 10);  // data, clock, latch
+
+void setup() {
+  display.begin(); //optional clear on startup
+}
+
+void loop() {
+  display.displayTxt("°c"); // text string, default offset
+  display.displayTxt("s.h.", 6); // text string, offset
+  display.displayNumber(-37, 2); // float/int value, offset
+}
